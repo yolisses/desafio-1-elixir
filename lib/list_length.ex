0a73f0667967d9e8c4570c76391b8054e4ace39a
Ulisses <m.stunik@gmail.com>
2021-10-18 20:55:49 -0300

    complete

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,13 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count_elements(list, 0)
+  end
+
+  def count_elements([head|tail], acc) do
+    count_elements(tail, acc+1)
+  end
+
+  def count_elements([], acc) do
+    acc
   end
 end
